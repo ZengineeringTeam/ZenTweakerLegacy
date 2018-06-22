119e4538818a0c5e74f8e9021c8b3698b90fd7ef
3TUSK <urey.s.knowledge@gmail.com>
2018-06-23 07:39:18 +0800

    Offhand

--- src/main/java/snownee/zentweaker/features/KeepMapOnDeath.java
+++ src/main/java/snownee/zentweaker/features/KeepMapOnDeath.java
@@ -3,6 +3,7 @@ package snownee.zentweaker.features;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumHand;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
@@ -32,6 +33,11 @@ public final class KeepMapOnDeath
                 {
                     event.getEntityPlayer().addItemStackToInventory(item);
                 }
+            }
+            ItemStack heldInOffHand = event.getOriginal().inventory.offHandInventory.get(0);
+            if (!heldInOffHand.isEmpty() && isMap(heldInOffHand))
+            {
+                event.getEntityPlayer().setHeldItem(EnumHand.OFF_HAND, heldInOffHand);
             }
         }
     }
