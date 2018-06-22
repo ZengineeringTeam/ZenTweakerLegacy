c4d1c548b5f8b8961ca6b9cb1b8ab35029ec130b
Snownee <1850986885@qq.com>
2018-06-23 03:27:58 +0800

    GG

--- src/main/java/snownee/zentweaker/features/KeepMapOnDeath.java
+++ src/main/java/snownee/zentweaker/features/KeepMapOnDeath.java
@@ -9,17 +9,29 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import snownee.zentweaker.ZenTweaker;
 
 @Mod.EventBusSubscriber(modid = ZenTweaker.MODID)
-public final class KeepMapOnDeath {
+public final class KeepMapOnDeath
+{
 
     @SubscribeEvent(priority = EventPriority.LOWEST)
-    public static void onPlayerDeath(PlayerEvent.Clone event) {
-        if (event.isWasDeath()) {
-            for (ItemStack item : event.getOriginal().inventory.mainInventory) {
-                if (!item.isEmpty() && (item.getItem() == Items.FILLED_MAP)) {
+    public static void onPlayerDeath(PlayerEvent.Clone event)
+    {
+        if (event.isWasDeath())
+        {
+            for (ItemStack item : event.getOriginal().inventory.mainInventory)
+            {
+                if (!item.isEmpty() && (item.getItem() == Items.FILLED_MAP || item.getItem() == Items.MAP
+                        || item.getItem().getRegistryName().toString().equals("antiqueatlas:antique_atlas")
+                        || item.getItem().getRegistryName().toString().equals("antiqueatlas:empty_antique_atlas")))
+                {
                     event.getEntityPlayer().addItemStackToInventory(item);
                 }
             }
         }
+    }
+
+    private static boolean isMap(ItemStack stack)
+    {
+        return false;
     }
 
 }
