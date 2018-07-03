69e37f7559bb6274e2a4883999d471456bace4b8
Snownee <1850986885@qq.com>
2018-07-03 18:14:49 +0800

    Fixed some logs are not recognized

--- src/main/java/snownee/zentweaker/features/NoTreePunching.java
+++ src/main/java/snownee/zentweaker/features/NoTreePunching.java
@@ -4,6 +4,9 @@ import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.NonNullList;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
@@ -22,25 +25,26 @@ public class NoTreePunching
     @SubscribeEvent
     public static void onPlayerBreaking(PlayerEvent.BreakSpeed event)
     {
-        if (isLog(event.getState()) && !canPunchTree(event.getEntityPlayer()))
+        event.getEntityPlayer().addItemStackToInventory(new ItemStack(event.getState().getBlock()));
+        if (isLog(event.getState(), event.getEntityPlayer().world, event.getPos()) && !canPunchTree(event.getEntityPlayer()))
         {
             event.setNewSpeed(event.getOriginalSpeed() / 4);
         }
     }
 
-    @SubscribeEvent
-    public static void onHarvestCheck(PlayerEvent.HarvestCheck event)
-    {
-        if (isLog(event.getTargetBlock()))
-        {
-            event.setCanHarvest(canPunchTree(event.getEntityPlayer()));
-        }
-    }
+    //    @SubscribeEvent
+    //    public static void onHarvestCheck(PlayerEvent.HarvestCheck event)
+    //    {
+    //        if (isLog(event.getTargetBlock()))
+    //        {
+    //            event.setCanHarvest(canPunchTree(event.getEntityPlayer()));
+    //        }
+    //    }
 
     @SubscribeEvent
     public static void onBlockDrops(BlockEvent.HarvestDropsEvent event)
     {
-        if (event.getHarvester() == null || !isLog(event.getState()))
+        if (event.getHarvester() == null || !isLog(event.getState(), event.getWorld(), event.getPos()))
         {
             return;
         }
@@ -64,14 +68,19 @@ public class NoTreePunching
         return !tool.isEmpty() && tool.getItem().getToolClasses(tool).contains("axe");
     }
 
-    public static boolean isLog(IBlockState state)
+    public static boolean isLog(IBlockState state, World world, BlockPos pos)
     {
-        ItemStack itemblock = new ItemStack(state.getBlock());
+        NonNullList<ItemStack> drops = NonNullList.create();
+        state.getBlock().getDrops(drops, world, pos, state, 0);
+        if (drops.size() < 1)
+        {
+            return false;
+        }
+        ItemStack itemblock = drops.get(0);
         if (itemblock.isEmpty())
         {
             return false;
         }
-
         for (int id : OreDictionary.getOreIDs(itemblock))
         {
             if (ORE_LOG_WOOD == id)
