144aa49405b81fadaa50aece5140d0307930ed03
Snownee <mjy523@126.com>
2018-12-30 02:22:15 +0800

    Merge flint tools into no tree punching

--- src/main/java/snownee/zentweaker/feature/NoTreePunching.java
+++ src/main/java/snownee/zentweaker/feature/NoTreePunching.java
@@ -2,20 +2,30 @@ package snownee.zentweaker.feature;
 
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.NonNullList;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry.ObjectHolder;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import net.minecraftforge.oredict.OreDictionary;
 import snownee.kiwi.IModule;
+import snownee.kiwi.Kiwi;
 import snownee.kiwi.KiwiModule;
+import snownee.kiwi.client.ModelUtil;
 import snownee.zentweaker.ZenTweaker;
+import snownee.zentweaker.item.ItemFlintAxe;
+import snownee.zentweaker.item.ItemFlintPickaxe;
+import snownee.zentweaker.item.ItemFlintShovel;
 
 @KiwiModule(modid = ZenTweaker.MODID, name = "NoTreePunching", optional = true)
 public class NoTreePunching implements IModule
@@ -25,9 +35,40 @@ public class NoTreePunching implements IModule
 
     private static final int ORE_LOG_WOOD = OreDictionary.getOreID("logWood");
 
+    private ItemFlintAxe itemFlintAxe;
+    private ItemFlintPickaxe itemFlintPickaxe;
+    private ItemFlintShovel itemFlintShovel;
+
     public NoTreePunching()
     {
         MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @Override
+    public void init()
+    {
+        OreDictionary.registerOre("stringAny", Items.STRING);
+        if (Kiwi.isOptionalModuleLoaded(ZenTweaker.MODID, "DropFibreAndFlint"))
+        {
+            OreDictionary.registerOre("stringAny", DropFibreAndFlint.PLANT_STRING);
+        }
+    }
+
+    @SubscribeEvent
+    public void onItemRegister(RegistryEvent.Register<Item> event)
+    {
+        event.getRegistry().register(itemFlintAxe = new ItemFlintAxe());
+        event.getRegistry().register(itemFlintPickaxe = new ItemFlintPickaxe());
+        event.getRegistry().register(itemFlintShovel = new ItemFlintShovel());
+    }
+
+    @SubscribeEvent
+    @SideOnly(Side.CLIENT)
+    public void onModelRegister(ModelRegistryEvent event)
+    {
+        ModelUtil.mapItemModel(itemFlintAxe);
+        ModelUtil.mapItemModel(itemFlintPickaxe);
+        ModelUtil.mapItemModel(itemFlintShovel);
     }
 
     @SubscribeEvent
