47ce13f178636e6498db87f843d6d662da46175b
Snownee <mjy523@126.com>
2019-01-02 03:23:14 +0800

    Add JEI helper plugin

---
+++ src/main/java/snownee/zentweaker/plugin/CategoryHidingPlugin.java
@@ -0,0 +1,24 @@
+package snownee.zentweaker.plugin;
+
+import mezz.jei.api.IJeiRuntime;
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.IRecipeRegistry;
+import mezz.jei.api.JEIPlugin;
+import mezz.jei.api.recipe.IRecipeCategory;
+
+@JEIPlugin
+public class CategoryHidingPlugin implements IModPlugin
+{
+    @Override
+    public void onRuntimeAvailable(IJeiRuntime jeiRuntime)
+    {
+        IRecipeRegistry registry = jeiRuntime.getRecipeRegistry();
+        for (IRecipeCategory category : registry.getRecipeCategories())
+        {
+            if (registry.getRecipeCatalysts(category).isEmpty())
+            {
+                registry.hideRecipeCategory(category.getUid());
+            }
+        }
+    }
+}
