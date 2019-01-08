b9385afd27ada56a2b2ca8c9a61c6799cb40772e
Snownee <mjy523@126.com>
2019-01-08 21:11:10 +0800

    New feature: TRWireRecipes

---
+++ src/main/java/snownee/zentweaker/feature/TRWireRecipes.java
@@ -0,0 +1,10 @@
+package snownee.zentweaker.feature;
+
+import snownee.kiwi.IModule;
+import snownee.kiwi.KiwiModule;
+import snownee.zentweaker.ZenTweaker;
+
+@KiwiModule(modid = ZenTweaker.MODID, name = "TRWireRecipes", optional = true, dependency = "industrialwires")
+public class TRWireRecipes implements IModule
+{
+}
