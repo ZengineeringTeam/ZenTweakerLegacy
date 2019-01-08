ab8b3362cffbfdc501aa470dc8369186a8e792d7
Snownee <mjy523@126.com>
2019-01-08 22:27:50 +0800

    New feature: NoGCBlueName

---
+++ src/main/java/snownee/zentweaker/feature/NoGCBlueName.java
@@ -0,0 +1,25 @@
+package snownee.zentweaker.feature;
+
+import net.minecraft.item.EnumRarity;
+import net.minecraftforge.fml.relauncher.ReflectionHelper;
+import snownee.kiwi.IModule;
+import snownee.kiwi.KiwiModule;
+import snownee.zentweaker.ZenTweaker;
+
+@KiwiModule(modid = ZenTweaker.MODID, name = "NoGCBlueName", optional = true, dependency = "galacticraftcore")
+public class NoGCBlueName implements IModule
+{
+    public NoGCBlueName()
+    {
+        Class clazz;
+        try
+        {
+            clazz = Class.forName("micdoodle8.mods.galacticraft.core.proxy.ClientProxyCore");
+            ReflectionHelper.setPrivateValue(clazz, null, EnumRarity.COMMON, "galacticraftItem");
+        }
+        catch (ClassNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
